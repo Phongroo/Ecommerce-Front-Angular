ac9c25efe2f25c47085d2d7ed19cf96ee78f7573
Phongroo <hongphong.12012001@gmail.com>
2023-05-16 09:04:03 +0700

    first commit

--- src/app/pages/admin/sidebar/sidebar.component.ts
+++ src/app/pages/admin/sidebar/sidebar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+  constructor(public loginservice:LoginService){}
+  ngOnInit(): void {
+   
+  }
+  logout(){
+  this.loginservice.logout();
+    window.location.reload();
+    // this.router.navigate(['/login']);
+   
+  }
 
 }
+
+
+
